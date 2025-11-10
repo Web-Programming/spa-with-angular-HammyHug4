2c0dd80941132f398a32d9971a7c0a9fdb565151
ShiroKoNeko-c <why2301p@gmail.com>
2025-11-10 17:44:07 +0700

    Angular

---
+++ griya-mdp/src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+import { Home as HomeComponent} from './home/home';
+import { Profile } from './profile/profile';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Contact } from './contact/contact';
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+        path: "profile",
+        component: Profile,
+        //title: 'Home Page'
+    },
+    {
+        path: "login",
+        component: Login,
+        //title: 'Home Page'
+    },
+    {
+        path: "register",
+        component: Register,
+        //title: 'Home Page'
+    },
+    {
+        path: "contact",
+        component: Contact,
+        //title: 'Home Page'
+    },
+];
--- my-cv-app/src/app/app.routes.server.ts
+++ my-cv-app/src/app/app.routes.server.ts
@@ -1,8 +1,35 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+// import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender
+//   }
+// ];
+import { Routes } from '@angular/router';
+import { CvComponent as CvComponent } from './cv/cv';
+import { ContactComponent as ContactComponent } from './contact/contact';
+
+export const routes: Routes = [
+ // 1. Path untuk Halaman CV
+ {
+   path: 'cv',
+   component: CvComponent,
+   title: 'Curriculum Vitae Saya'
+ },
+ // 2. Path untuk Halaman Kontak
+ {
+   path: 'contact',
+   component: ContactComponent,
+   title: 'Kontak Saya'
+ },
+ // 3. Redirect halaman utama ke /cv
+ {
+   path: '',
+   redirectTo: '/cv',
+   pathMatch: 'full'
+ },
+ // 4. (Opsional) Halaman 404
+ // { path: '**', component: NotFoundComponent }
 ];
+
