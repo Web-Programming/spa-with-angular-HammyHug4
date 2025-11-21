f9c381ea2c985ff9892417deb80158e5f727b484
ShiroKoNeko-c <why2301p@gmail.com>
2025-11-21 17:03:24 +0700

    implementasi backend

---
+++ griya-mdp-backend/app_server/models/db.js
@@ -0,0 +1,18 @@
+let mongoose = require("mongoose");
+// let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
+// let dbURI = "mongodb://localhost:27017/namadb"
+const dbURI = ('mongodb://localhost:27017/paw2A')
+
+mongoose.connect(dbURI, {});
+
+mongoose.connection.on("connected", () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
